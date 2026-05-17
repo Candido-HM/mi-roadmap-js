9a7048b2e310cab4a048fabb1161726897630d80
Candido Hernandez <candidohm99cursos@gmail.com>
2026-05-17 11:58:25 -0600

    practica de ejercicios con el ciclo for

---
+++ 04-Ciclos/05-Ejercicios-for.js
@@ -0,0 +1,13 @@
+// Ejercicio la cuenta regresiva
+
+for ( let contador = 5; contador >= 1; contador--) {
+    console.log(`Lanzamiento en: ${contador}`);
+}
+console.log("¡Lanzamiento exitoso! 🚀"); 
+
+// El generador de muestrario
+const nameProduct = 'Mini vela Arómatica';
+for ( let numberEtiqueta = 1; numberEtiqueta <= 4; numberEtiqueta++) {
+    console.log(`Etiqueta #${numberEtiqueta}: ${nameProduct}`);
+}
+console.log("Proceso de etiquetado finalizado");
