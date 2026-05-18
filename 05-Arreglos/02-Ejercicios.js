0be094673c1ebd4d0bcce32b565093df556d22c5
Candido Hernandez <candidohm99cursos@gmail.com>
2026-05-18 13:38:40 -0600

    ejercicios de arreglos

--- 05-Arreglos/02-Ejercicios.js
+++ 05-Arreglos/02-Ejercicios.js
@@ -11,3 +11,27 @@ por cada material*/
 for (let i = 0; i < materiales.length; i++) {
     console.log(`Material necesario: ${materiales[i]}`);
 }
+
+// Ejercicio -> El Filtro de Alertas
+const medidasCirios = [10, 7.5, 9, 6.8, 11];
+
+for (let cirios = 0; cirios < medidasCirios.length; cirios++) {
+    let medida = medidasCirios[cirios];
+    // console.log(`medias cirios: ${medida}`);
+    //aqui va ir un if else para validar medidas
+    if ( medida < 8) {
+        console.log(`Alerta: Cirio de ${medida}cm no pasa el control de calidad.`)
+    } else {
+        console.log(`Cirio de ${medida}cm aprobado`);
+    }
+}
+
+// Ejercicio el contador de capital
+const costos = [150, 45, 80, 210];
+let totalGastado = 0;
+
+for (let c = 0; c < costos.length; c++) {
+    // totalGastado = totalGastado + costos[c];
+    totalGastado += costos[c];
+}
+console.log(`El gasto total de la semana fue de: ${totalGastado}`);
